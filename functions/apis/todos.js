6cb876f2ddb94542ba56790c2a191ea5f5e1f7cc
Richard Clarke <freshnewrich@gmail.com>
2022-08-06 16:23:55 -0700

    update todos with database conn

--- functions/apis/todos.js
+++ functions/apis/todos.js
@@ -1,17 +1,45 @@
 //todos.js
 
-exports.getAllTodos = (req, res) => {
-    todos = [
-        {
-            'id':'1',
-            'title':'greeting',
-            'body':"hello. is it me you're looking for?"
-        },
-        {
-            'id':'2',
-            'title':'another todo',
-            'body':'i have a lot to do today'
-        }
-    ]
-    return res.json(todos)
-}
+//Original version
+// exports.getAllTodos = (req, res) => {
+//     todos = [
+//         {
+//             'id':'1',
+//             'title':'greeting',
+//             'body':"hello. is it me you're looking for?"
+//         },
+//         {
+//             'id':'2',
+//             'title':'another todo',
+//             'body':'i have a lot to do today'
+//         }
+//     ]
+//     return res.json(todos)
+// }
+
+//todos.js
+
+const { db } = require('../util/admin');
+
+exports.getAllTodos = (request, response) => {
+	db
+		.collection('todos')
+		.orderBy('createdAt', 'desc')
+		.get()
+		.then((data) => {
+			let todos = [];
+			data.forEach((doc) => {
+				todos.push({
+                    todoId: doc.id,
+                    title: doc.data().title,
+					body: doc.data().body,
+					createdAt: doc.data().createdAt,
+				});
+			});
+			return response.json(todos);
+		})
+		.catch((err) => {
+			console.error(err);
+			return response.status(500).json({ error: err.code});
+		});
+};
