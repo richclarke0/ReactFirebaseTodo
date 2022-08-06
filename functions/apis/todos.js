5c8e4dde0d7d4d0a0da1c7261663cedeeb5ead0b
Richard Clarke <freshnewrich@gmail.com>
2022-08-06 11:52:28 -0700

    new todos.js and content

---
+++ functions/apis/todos.js
@@ -0,0 +1,17 @@
+//todos.js
+
+exports.getAllTodos = (req, res) => {
+    todos = [
+        {
+            'id':'1',
+            'title':'greeting',
+            'body':"hello. is it me you're looking for?"
+        },
+        {
+            'id':'2',
+            'title':'another todo',
+            'body':'i have a lot to do today'
+        }
+    ]
+    return Response.json(todos)
+}
