02a2ae86d1d3e478b207e0c7be9092b9dda76416
Richard Clarke <freshnewrich@gmail.com>
2022-08-06 11:28:08 -0700

    new route in index

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,26 @@
-const functions = require('firebase-functions');
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-     response.send("Hello from Firebase!");
-});
-
+//the beginning
+// const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+     //   functions.logger.info("Hello logs!", {structuredData: true});
+     //   response.send("Hello from Firebase!");
+     // });
+     
+//first export
+// const functions = require('firebase-functions');
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//      response.send("Hello from Firebase!");
 // });
+
+//next thing
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
