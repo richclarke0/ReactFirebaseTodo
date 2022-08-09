23196180d721d74a6a9fbb86650182d9265db66b
Richard Clarke <freshnewrich@gmail.com>
2022-08-09 13:06:46 -0700

    validators.js

---
+++ functions/util/validators.js
@@ -0,0 +1,14 @@
+// validators.js
+
+//cleaned this function up a bit from the original
+const isEmpty = (string) => !string.trim()
+
+exports.validateLoginData = (data) => {
+   let errors = {};
+   if (isEmpty(data.email)) errors.email = 'Must not be empty';
+   if (isEmpty(data.password)) errors.password = 'Must not be  empty';
+   return {
+       errors,
+       valid: Object.keys(errors).length === 0 ? true : false
+    };
+};
